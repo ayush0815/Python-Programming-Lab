3824b98e8459c65e4d00cd79c4d1842606c7f9cc
Ayush Srivastava <91680234+ayush0815@users.noreply.github.com>
2022-05-30 19:57:31 +0530

    Dictionary

---
+++ Info.py
@@ -0,0 +1,9 @@
+a ={
+'Name': 'Ayush Srivastava',
+'Section': 'F',
+'Course': 'B.Tech CSE',
+'Roll Number' : 14
+}
+
+
+print (a)
---
+++ info by user.py
@@ -0,0 +1,12 @@
+nm = input('Enter the Name:')
+rolln = int(input('Enter the Roll Number:'))
+crs = input('Enter the course Name:')
+sec = input('Enter the section:')
+
+x = f'''
+Name: {nm}
+Section: {sec}
+Course: {crs}
+Roll Number: {rolln}'''
+
+print(x)
